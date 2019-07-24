2b064fe3bb5b75127d7538c6b1bff8931f9692c0
elefcodes <panteliselef@outlook.com>
2019-07-24 23:15:11 +0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -27,6 +28,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Lato:300,400,500']
+        }
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,54 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import illustration from '../images/comming-soon-illustration.svg';
+import logo from '../images/envelope-logo.svg';
+import styled from 'styled-components';
+import '../styles/index.css';
+// import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+	const Container = styled.div`
+		display: flex;
+		${'' /* justify-content: center; */} align-items:center;
+		flex-direction: column;
+		width: 100%;
+		padding: 2rem 0;
+    height: 100vh;
+    max-height: 100vh;
+	`;
 
-export default IndexPage
+	const TextContainer = styled.div`
+		max-width: 650px;
+		text-align: center;
+    font-size: 1.5rem;
+  `;
+  
+
+	// <Layout>
+	//   <SEO title="Home" />
+	//   <h1>Hi people</h1>
+	//   <p>Welcome to your new Gatsby site.</p>
+	//   <p>Now go build something great.</p>
+	//   <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+	//     <Image />
+	//   </div>
+	//   <Link to="/page-2/">Go to page 2</Link>
+	// </Layout>
+
+	return (
+		<Container>
+			<img src={logo} style={{ width: '100%', height: '2rem' }} />
+
+			<TextContainer>
+				<p style={{fontWeight:'300'}}>Looks like nobody is home!</p>
+				<p style={{fontWeight:'500',color:'#000'}}>Our website is under construction, please check back later</p>
+			</TextContainer>
+      <img src={illustration} style={{width: 'auto', height: 'calc(100vh - 30%)', objectFit:'contain',flexGrow: 1}} />
+		</Container>
+	);
+};
+
+export default IndexPage;
